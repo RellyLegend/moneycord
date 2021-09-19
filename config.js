7bb23c8496154b1b32cb91407b69a2da16cc9864
RellyLegend <75770996+RellyLegend@users.noreply.github.com>
2021-09-19 19:34:19 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    mongo: "mongodb+srv://lexonbot:Q4VGmScusTp2TzG@cluster0.ykska.mongodb.net/DiscordEconomyBot?authSource=admin&replicaSet=atlas-12kig6-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    token: "ODc2MDY2MTYyOTY0ODUyNzU3.YReqMQ.gTe0gsEZEb9w_M3RgdpOHHUbT0Y"
+}    
